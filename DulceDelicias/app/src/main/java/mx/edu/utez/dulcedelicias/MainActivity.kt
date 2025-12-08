6b4a2fa614a97823e735c7ad630066be0d85f7e4
TaniaSR8 <20243ds011@utez.edu.mx>
2025-12-08 11:05:14 -0600

    Se agrego lwa estructura del proyecto

---
+++ DulceDelicias/app/src/main/java/mx/edu/utez/dulcedelicias/MainActivity.kt
@@ -0,0 +1,27 @@
+package mx.edu.utez.dulcedelicias
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import mx.edu.utez.dulcedelicias.ui.theme.DulceDeliciasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DulceDeliciasTheme {
+
+            }
+        }
+    }
+}
+
