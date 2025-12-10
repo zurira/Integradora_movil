5a62d7ed1b9ccacea8224e75de1688f94406432a
zurira <20243ds036@utez.edu.mx>
2025-12-10 17:51:23 -0600

    Se definio el modelo de producto

---
+++ DulceDelicias/app/src/main/java/mx/edu/utez/dulcedelicias/data/network/model/Producto.kt
@@ -0,0 +1,8 @@
+package mx.edu.utez.dulcedelicias.data.network.model
+data class Producto(
+    val id: Int,
+    val nombre: String,
+    val descripcion: String,
+    val precio: Double,
+    val imagenUrl: String
+)
