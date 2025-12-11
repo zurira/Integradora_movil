7732ed8fa2d7f24eafff72a59c8e248110e9096c
zurira <20243ds036@utez.edu.mx>
2025-12-10 18:18:08 -0600

    Definicii´ón del modelo UsuarioAdmin

---
+++ DulceDelicias/app/src/main/java/mx/edu/utez/dulcedelicias/data/network/model/UsuarioAdmin.kt
@@ -0,0 +1,9 @@
+package mx.edu.utez.dulcedelicias.data.network.model
+
+data class UsuarioAdmin (
+    val id: Int,
+    val nombre: String,
+    val correo: String,
+    val usuario: String,
+    val contraseña: String
+)
