ec0c1c65eab48d80382fd51bbbb9fca46fe5c706
Daniela <20243ds043@utez.edu.mx>
2025-12-11 08:54:13 -0600

    Archivos

--- DulceDelicias/app/src/main/java/mx/edu/utez/dulcedelicias/ui/screens/EditarPerfilScreen.kt
+++ DulceDelicias/app/src/main/java/mx/edu/utez/dulcedelicias/ui/screens/EditarPerfilScreen.kt
@@ -5,9 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
